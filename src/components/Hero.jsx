31b775545f7d7aa1e5183907df93a4f9428b2df2
Dev <devkatyal@protonmail.com>
2024-08-14 01:55:25 -0400

    updated the description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,8 @@ export default function Hero(){
             <div className='hs-1'>
                 <h1>Greater Toronto Area’s Mobile Detailing Experts</h1>
                 <div className='hs-line'>
-                    <p>Experience the difference of a professional car detail without leaving your driveway, transforming your car with meticulous cleaning and a 100% satisfaction guarantee.</p>
+                    <p>Professional Car Detailers servicing the Durham Region and surrounding areas. Half a decade of car detailing expertise right at your door, at Elite Auto Aesthetics we go above the standard to ensure your vehicle is left in pristine condition but most importantly we value our clients’ experience with us. 
+</p>
                 </div>
                 <div className='hs-cta'>
                     <a href='tel:647-561-6777'>Give us a Call<i class="fa-solid fa-phone"></i></a>
