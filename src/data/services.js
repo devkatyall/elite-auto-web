4cef38f8a7e13fa4ce2500455f5c429572a75b46
Dev <devkatyal@protonmail.com>
2024-08-22 01:41:37 -0400

    changes to the price section

--- src/data/services.js
+++ src/data/services.js
@@ -1,24 +1,8 @@
  const services = [
     {
-      title: "Express Detail",
-      priceReg: 60,
-      priceBig: 80,
-      info: [
-        "2 Bucket Wash",
-        "Wheel Deep Cleaned",
-        "Towel dry",
-        "Windows cleaned",
-        "Door and Trunk Jambs cleaned",
-        "Interior blow out",
-        "Full Deep Vacuum",
-        "Interior Wipe Down",
-        "Air Freshner"
-      ]
-    },
-    {
       title: "Full Interior Detail",
-      priceReg: 80,
-      priceBig: 100,
+      priceReg: 120,
+      priceBig: 180,
       info: [
         "Door Panels, Dash and Console Properly Deep Cleaned and Scrubbed Down",
         "Steam Cleaning Treatment On Interior Cup Holders, A/C Vents and Crevices",
@@ -30,8 +14,8 @@
     },
     {
       title: "Full Detail",
-      priceReg: 100,
-      priceBig: 120,
+      priceReg: 150,
+      priceBig: 220,
       info: [
         "STANDARD EXTERIOR DETAIL + Iron removal treatment",
         "Clay bar treatment",
@@ -45,29 +29,8 @@
       titleAdd: "Add On Services",
       infoAdd: [
         "Headlight Restoration: $20 (Each Light)",
-        "Buffing: $50-$200 (Depending on quantity)",
-        "Polishing: $50-$100",
-        "Waxing: $50",
-        "Mold Removal: $150+ (Depends on quantity)"
-      ]
-    },
-    {
-      title: "Golden Full Detail Package",
-      priceReg: 220,
-      priceBig: 250,
-      info: [
-        "+ENGINE BAY DETAIL 1ST ​DETAIL",
-        "+UNDER CARRIAGE ClEAN UP ​1ST DETAIL",
-        "GUARANTEED 2 FREE FULL ​DETAILS AFTER THE FIRST ​TWO",
-        "2 FREE DETAILS AFTER THE ​FIRST TWO REGARDLESS OF ​CONDITION/ FIXED PRICE"
-      ]
-    },
-    {
-      title: "Golden Express Detail Package",
-      priceReg: 450,
-      priceBig: 650,
-      info: [
-        "12 EXPRESS WASHES ​ANNUALLY",
+        "Mold Removal: $150+ (Depends on quantity)",
+        "Ceramic Coating: $400+"
       ]
     }
   ];
